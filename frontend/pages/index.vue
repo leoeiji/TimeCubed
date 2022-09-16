6c816a79b7cd785e1421d0e61f334714d27e3739
leoeiji <leoeiji1234@gmail.com>
2022-09-16 18:52:23 -0300

    Stopwatch and scramble generator added

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -10,13 +10,24 @@
         ></v-data-table>
       </v-col>
 
-      <v-col cols="8">
-        <StopWatch
-          ref="stopWatch"
-          class="digits"
-          minutes
-          :hours="false"
-        />
+      <v-col 
+        cols="8" 
+        class="d-flex justify-center align-center flex-column"
+      >
+        <div class="d-flex justify-center align-center flex-column">
+          <StopWatch
+            ref="stopWatch"
+            :class="{'display-4': true, 'green--text': pressProgress == 100}"
+            minutes
+          />
+          <v-progress-linear
+            background-color="transparent"
+            rounded
+            :value="pressProgress"
+            :color="pressProgress == 100 ? 'green' : 'white'"
+          ></v-progress-linear>
+        </div>
+        <span v-show="!spaceDown && !$refs.stopWatch.isRunning" class="display-1 mt-4"> {{ scramble }} </span>
       </v-col>
     </v-row>
   </v-container>
@@ -29,19 +40,23 @@ export default {
   data() {
     return {
       spaceDown: false,
-      spaceDownTime: null,
+      spaceDownInterval: null,
+      pressProgress: 0,
+      scramble: this.getScramble().join(' '),
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Time',
           align: 'start',
-          sortable: false,
-          value: 'name',
+          value: 'time',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
+        {
+          text: 'Ao5',
+          value: 'ao5',
+        },
+        {
+          text: 'Ao12',
+          value: 'ao12',
+        }
       ],
 
       records: [
@@ -52,24 +67,59 @@ export default {
   
   methods: {
     onSpaceDown() {
-      this.spaceDown = true;
-      this.spaceDownTime = performance.now();
+      this.spaceDown = true
+      this.spaceDownInterval = setInterval(() => {
+        if (this.pressProgress < 100) this.pressProgress = this.pressProgress + 10
+      }, 100)
     },
     
     onSpaceUp() {
       this.spaceDown = false
-      if (performance.now() - this.spaceDownTime > 1000) {
-        this.$refs.stopWatch.start();
-      } else {
-        if (this.$refs.stopWatch.isRunning) {
-          this.$refs.stopWatch.stop();
-        }
-      }      
+      clearInterval(this.spaceDownInterval)
+      if (this.pressProgress >= 100) {
+        this.$refs.stopWatch.start()
+      } else if (this.$refs.stopWatch.isRunning) {
+        this.$refs.stopWatch.stop()
+        this.records.push({
+          time: this.$refs.stopWatch.time,
+          ao5: '',
+          ao12: '',
+        })
+        this.scramble = this.getScramble().join(' ')
+      }
+      this.pressProgress = 0
     },
+
+    getScramble() {
+      const directions = [
+        ["D", "U"],
+        ["L","R"],
+        ["F","B"]
+      ];
+
+      const times = ["", "'", "2"];
+
+      const random = (array, exclude) => {
+        let n;
+        do {
+            n = Math.floor( Math.random() * array.length );
+        } while(array[n] === exclude)
+        return array[n];
+      }
+
+      const scramble = new Array(20);
+      let direction;
+      for(let i = 0; i < scramble.length; i++){
+        direction = random(directions, direction);
+        scramble[i] = random(direction) + random(times);
+      }
+
+      return scramble;
+    }
   },
 
   mounted() {
-    window.addEventListener('keydown', (e) => {
+    window.addEventListener('keypress', (e) => {
       if (e.code === 'Space') {
         if (!this.spaceDown) {
           this.onSpaceDown();
